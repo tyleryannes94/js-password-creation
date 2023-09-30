0850dea74974d89ca113bf5b41ea68334a621073
tyleryannes94 <tyler.yannes@gmail.com>
2023-09-30 11:59:47 -0500

    Added a function for generatePassword to be used in the writePassword function provided.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  var length = 16;
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  var password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
