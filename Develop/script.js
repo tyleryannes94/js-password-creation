ef5c088b94fe35ba16f358b62a6a20ee4c33571f
tyleryannes94 <tyler.yannes@gmail.com>
2023-09-30 17:52:17 -0500

    added prompt message when clicking the generate password button.

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,9 @@ function generatePassword () {
     for (var i = 0, n = charset.length; i < length; ++i) {
         password += charset.charAt(Math.floor(Math.random() * n));
     }
+
+  var passwordLength = prompt ("Input an integer for how many characters your password should be.")
+  
   return password;
 }
 
